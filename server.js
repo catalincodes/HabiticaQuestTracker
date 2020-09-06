8676d80b6a9615b7164cebe5ac794d8590c09e09
Catalin Popescu <catalinp86@gmail.com>
2020-09-06 15:55:51 -0400

    Clean code - Add ESLint - Clean using Google's JS coding style

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 const db = require('./db');
 
 app.use(express.static(__dirname));
@@ -13,34 +13,33 @@ app.use(bodyParser.urlencoded({extended: false}));
 mongoose.Promise = Promise;
 
 app.get('/messages', (req, res) =>{
-    db.getAllMessages( (messages) => res.json(messages));
-})
+  db.getAllMessages( (messages) => res.json(messages));
+});
 
 app.get('/messages/:user', (req, res) =>{
-    var user = req.params.user;
-
-    db.getAllMessagesFromUser(user, (messages) => res.json(messages))
-})
+  const user = req.params.user;
+  db.getAllMessagesFromUser(user, (messages) => res.json(messages));
+});
 
 app.post('/messages', async (req, res) => {
-    try {
-        db.addNewMessage(req.body);    
-        io.emit('message', req.body);
-        res.sendStatus(200);
-    } catch (error) {
-        res.sendStatus(500);
-        return console.error(error);
-    } finally {
-        console.log('message posted called')
-    }
-})
+  try {
+    db.addNewMessage(req.body);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+    return console.error(error);
+  } finally {
+    console.log('message posted called');
+  }
+});
 
 io.on('connection', (socket) => {
-    console.log(' a user connected')
-})
+  console.log(' a user connected');
+});
 
 db.connect();
 
-var server = http.listen(process.env.PORT || 3000, () => {
-    console.log('server is listening on port', server.address().port)
-})
+const server = http.listen(process.env.PORT || 3000, () => {
+  console.log('server is listening on port', server.address().port);
+});
